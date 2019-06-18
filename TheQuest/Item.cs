8351852c9bf317d32fd38c54a55370d316786c0d
Threak <kaspar-pawlak@hotmail.com>
2019-06-19 01:59:45 +0200

    Models for Item, stats and enemy classes

--- TheQuest/Item.cs
+++ TheQuest/Item.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace TheQuest.Models
 {
     public class Item
     {
+        [Key]
+        public int ItemId { get; set; }
+
+        public string Name { get; set; }
+
+        public int StatsId { get; set; }
+
+        public Stats Stats { get; set; }
+
+        public int LevelReq { get; set; }
     }
 }
